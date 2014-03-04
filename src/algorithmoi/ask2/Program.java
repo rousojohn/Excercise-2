0ebf5305f277dfe9ce9749a35d6ab15a487df9e8
rousojohn <rousojohn@gmail.com>
2014-03-04 23:36:08 +0200

    Preparation for 2nd phase
    Read the 2 Lists given by the user.
    Sort them.

--- src/algorithmoi/ask2/Program.java
+++ src/algorithmoi/ask2/Program.java
@@ -1,9 +1,11 @@
 package algorithmoi.ask2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Scanner;
 
 import algorithmoi.ask2.parallel.MultiThreadedSearcher;
 import algorithmoi.ask2.sequential.Searcher;
@@ -12,10 +14,79 @@ public class Program implements Observer {
 	private static List<Integer> array;
 	
 	MultiThreadedSearcher searcher;
+	private List<Integer> listX;
+	private List<Integer> listY;
 
 	public static void main(String[] args) {
 		Program p = new Program();
+		
+		// Initialize the local variables holding the 2 lists to be merged
+		p.listX = new ArrayList<Integer>();
+		p.listY = new ArrayList<Integer>();
+
+		// Initialize the Input Stream
+		Scanner mScanner = new Scanner(System.in);
+		
+		// Flag to determine whether or not to read another integer for the current ArrayList
+		boolean proceed = true;
+
+		//		Read the 1st List
+		do{
+			System.out.println("Give The First List (X) ");
+			
+			// Get the integer and add it to the list
+			p.listX.add(mScanner.nextInt());
+			
+			// Determine whether to continue reading integers or not
+			System.out.println("Do you want to continue? Y/N ");
+			String option = mScanner.next();
+			proceed = (option.equals("y") || option.equals("Y"));
+		}while (proceed == true);
+		
+		
+		proceed = true;
+
+		//		Read the 2nd List
+		do{
+			System.out.println("Give The Second List (Y) ");
+			
+			// Get the integer and add it to the list
+			p.listY.add(mScanner.nextInt());
+			
+			// Determine whether to continue reading integers or not
+			System.out.println("Do you want to continue? Y/N ");
+			String option = mScanner.next();
+			proceed = (option.equals("y") || option.equals("Y"));
+		}while (proceed == true);
+		
+		// Make sure the lists are sorted.
+		p.sortList(p.listX);
+		p.sortList(p.listY);
+	}
+
+	
+	/**
+	 * Overrides the update method inherited by parent class Observer.
+	 * It is called whenever an Observable class calls notifyObservers()
+	 */
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		System.out.println(((MultiThreadedSearcher)arg0).getIndex());		
+	}
+	
+	
+	private void sortList(List<Integer> _list){
+		Collections.sort(_list);
+	}
+	
+	public static void testFirstPart(){
+		Program p = new Program();
 		array = new ArrayList<Integer>() {
+			/**
+			 * Auto-generated serial UID 
+			 */
+			private static final long serialVersionUID = 1L;
+
 			{ 
 				add(1);
 				add(2);
@@ -41,21 +112,6 @@ public class Program implements Observer {
 		
 		// finally run the the threads
 		p.searcher.runAllThreads();
-	}
-	
-	
-	public void run(){
-	}
-
-
-	
-	/**
-	 * Overrides the update method inherited by parent class Observer.
-	 * It is called whenever an Observable class calls notifyObservers()
-	 */
-	@Override
-	public void update(Observable arg0, Object arg1) {
-		System.out.println(((MultiThreadedSearcher)arg0).getIndex());		
 	}
 
 }
